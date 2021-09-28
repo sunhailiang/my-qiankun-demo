a4b1ed233afd851420efb2a2536c5fd30273cd2b
hailiang.sun <peng6239528@126.com>
2021-09-28 23:40:21 +0800

    写了一点

--- micro-main/src/App.vue
+++ micro-main/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-menu :router="true" mode="horizontal">
+      <!-- 自己的路由 -->
+      <el-menu-item index="/">主框架路由</el-menu-item>
+      <!-- 子应用的路由 -->
+      <el-menu-item index="part-one">第一子应用</el-menu-item>
+      <el-menu-item index="part-two">第二子应用</el-menu-item>
+    </el-menu>
+    <router-view></router-view>
+    <div id="part-one"></div>
+    <div id="part-two"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
